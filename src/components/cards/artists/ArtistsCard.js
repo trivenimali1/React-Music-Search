f8d04e32b313b7d3882004f94335f66d75b4e246
Triveni <trivenimali14j@gmail.com>
2020-04-15 14:22:19 +0530

    Changes made in files

--- src/components/cards/artists/ArtistsCard.js
+++ src/components/cards/artists/ArtistsCard.js
@@ -22,9 +22,7 @@ class ArtistsCard extends Component {
 
         let audios = document.getElementsByTagName('audio');
         for (let i = 0, len = audios.length; i < len; i++) {
-            // if(audios[i] != e.target){
             audios[i].pause();
-            //}
         }
     }
 
